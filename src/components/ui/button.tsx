1395efaebfded20e36256fb7bccb37623ab838ad
Nikita <kozlovzxc@gmail.com>
2026-01-04 23:06:51 +0000

    button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,20 +9,20 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground border-primary hover:opacity-90",
-        secondary: "bg-secondary text-secondary-foreground border-primary hover:bg-background",
-        destructive: "bg-destructive text-destructive-foreground border-destructive hover:opacity-90",
+        default: "bg-primary text-primary-foreground border-primary hover:opacity-80",
+        secondary: "bg-secondary text-secondary-foreground border-primary hover:bg-muted",
+        destructive: "bg-red-100 text-red-700 border-red-500 hover:bg-red-200",
       },
       size: {
         default:
           "h-8 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2",
-        xs: "h-6 gap-1 px-2 text-xs has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3",
-        sm: "h-7 gap-1 px-2.5 has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3.5",
-        lg: "h-9 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-3 has-data-[icon=inline-start]:pl-3",
+        xs: "h-5 gap-0.5 px-1.5 text-[10px] has-data-[icon=inline-end]:pr-1 has-data-[icon=inline-start]:pl-1 [&_svg:not([class*='size-'])]:size-2.5",
+        sm: "h-6 gap-1 px-2 has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3",
+        lg: "h-11 gap-2 px-4 text-sm has-data-[icon=inline-end]:pr-3 has-data-[icon=inline-start]:pl-3 [&_svg:not([class*='size-'])]:size-5",
         icon: "size-8",
-        "icon-xs": "size-6 [&_svg:not([class*='size-'])]:size-3",
-        "icon-sm": "size-7",
-        "icon-lg": "size-9",
+        "icon-xs": "size-5 [&_svg:not([class*='size-'])]:size-2.5",
+        "icon-sm": "size-6 [&_svg:not([class*='size-'])]:size-3",
+        "icon-lg": "size-11 [&_svg:not([class*='size-'])]:size-5",
       },
     },
     defaultVariants: {
