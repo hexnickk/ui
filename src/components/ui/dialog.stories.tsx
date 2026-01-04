b9b04260a0afb8ed0c233269217c190ad0cc85be
Nikita <kozlovzxc@gmail.com>
2026-01-04 23:21:02 +0000

    dialog

---
+++ src/components/ui/dialog.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from './dialog';
+import { Button } from './button';
+
+const meta: Meta<typeof Dialog> = {
+  title: 'UI/Dialog',
+  component: Dialog,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Dialog>;
+
+export const Default: Story = {
+  render: () => (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Delete Account</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Confirm Deletion</DialogTitle>
+          <DialogDescription>
+            Are you sure you want to delete your account? This action cannot be undone and all your data will be permanently removed.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button variant="secondary">Cancel</Button>
+          <Button variant="destructive">Delete Account</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  ),
+};
